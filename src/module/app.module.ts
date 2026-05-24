ac8fdd1f2a3ae36a96b8975f26c1c79ae618e23d
Raheem Adam <raheemadam85@gmail.com>
2026-05-24 13:14:15 +0100

    initial commit

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -36,16 +36,18 @@ import { DatabaseService } from '../database/database-service';
       isGlobal: true,
     }),
    
+
     TypeOrmModule.forRoot({
         
-       type: 'postgres',
-       host: process.env.DB_HOST,
-       port: 5432,
-       username: process.env.DB_USERNAME,
-       password: process.env.DB_PASSWORD,
-       database: process.env.DB_NAME,
-        
-         ssl: { rejectUnauthorized: false },
+      type: 'postgres',
+      port: Number(process.env.DB_PORT),
+      url: process.env.DATABASE_URL,
+ 
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
     
         entities: [
           UserEntity, ProductEntity, CategoryEntity, CartEntity, CartItem,
